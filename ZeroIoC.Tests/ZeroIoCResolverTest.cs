b882bf4d3fd34f98bcb8b1c3e3a5f617ccab8e47
Stanislav Silin <silin.stanislav@outlook.com>
2021-07-30 23:43:29 +0300

    Add initial transient support.

--- ZeroIoC.Tests/ZeroIoCResolverTest.cs
+++ ZeroIoC.Tests/ZeroIoCResolverTest.cs
@@ -61,5 +61,41 @@ namespace ZeroIoC.Tests
 
             Assert.IsTrue(firstService != null && secondService != null && firstService.Equals(secondService));
         }
+
+        [TestMethod]
+        public async Task SimpleTransient()
+        {
+            var project = await TestProject.Project.ApplyToProgram(@"
+
+        public interface IService
+        {
+
+        }
+
+        public class Service : IService
+        {
+
+        }
+
+        public partial class TestContainer : ZeroIoCContainer
+        {
+            protected override void Bootstrap(IZeroIoCContainerBootstrapper bootstrapper)
+            {
+                bootstrapper.AddTransient<IService, Service>();
+            }
+        }
+");
+
+            var newProject = await project.ApplyZeroIoCGenerator();
+
+            var assembly = await newProject.CompileToRealAssembly();
+            var containerType = assembly.GetType("TestProject.TestContainer");
+            var serviceType = assembly.GetType("TestProject.IService");
+            var container = Activator.CreateInstance(containerType);
+            var firstService = container.ReflectionCall("GetService", serviceType);
+            var secondService = container.ReflectionCall("GetService", serviceType);
+
+            Assert.IsTrue(firstService != null && secondService != null && !firstService.Equals(secondService));
+        }
     }
 }
